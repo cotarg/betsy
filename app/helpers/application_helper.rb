8329d840f01e15f67eb8b85c1dd101ec357504b9
Lisa Rolczynski <lisa.sano@gmail.com>
2016-05-26 16:04:05 -0700

    attempting to add estimate price into shipping_methods as a hash serialized to json

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,4 @@
+require 'json'
+
 module ApplicationHelper
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ActiveRecord::Base
   has_many :orderitems
   belongs_to :user
+
+  serialize :shipping_method, JSON
 end
