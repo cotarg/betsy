9a1dce6b63398235804f45df430581161cab55f5
Suzanne Harrison <harrison_family@hotmail.com>
2016-05-27 14:56:31 -0700

    fixing index in application controller for json method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ require 'json'
 
 module ApplicationHelper
   def json_shipping_value
-    number_with_precision (JSON.parse(@order.shipping_method).values.first / 100.0) , precision: 2
+    number_with_precision (JSON.parse(@order.shipping_method).values.first.first / 100.0) , precision: 2
   end
 end
